257ae510487e2213b521a32115508946f2643d2d
Jenga05 <80204216+Jenga05@users.noreply.github.com>
2022-09-02 09:09:26 +0200

    initial commit

---
+++ Vizsga/src/main/java/com/spring/vizsga/VizsgaBodoReginaApplication.java
@@ -0,0 +1,13 @@
+package com.spring.vizsga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VizsgaBodoReginaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VizsgaBodoReginaApplication.class, args);
+	}
+
+}
